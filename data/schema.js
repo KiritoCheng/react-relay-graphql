208b94bae5c3690f4a8fdd4857c4c25bd23e3683
Kirito <z1345036885@163.com>
2018-11-15 10:29:02 +0800

    update

--- data/schema.js
+++ data/schema.js
@@ -1,46 +1,79 @@
 import {
     GraphQLString,
+    GraphQLInt,
     GraphQLObjectType,
     GraphQLSchema
 } from 'graphql';
 import {
     nodeDefinitions,
-    globalIdField,
     fromGlobalId,
     connectionDefinitions
 } from 'graphql-relay';
+import { Hello } from './database'
 
 const { nodeInterface, nodeField } = nodeDefinitions(
     globalId => {
         var { type, id } = fromGlobalId(globalId);
-        return data[type][id]
+        return null;
     },
     obj => {
-        return GraphQLHello;
+        return AddressType;
     }
 )
 
-const GraphQLHello = new GraphQLObjectType({
-    name: 'Hello',
+const AddressType = new GraphQLObjectType({
+    name: 'Address',
+    description: '这是一个住址',
     fields: {
-        id: globalIdField()
+        street: {
+            type: GraphQLString,
+            resolve: () => '深圳市'
+        },
+        number: {
+            type: GraphQLInt,
+            resolve: () => 555000
+        },
+        formatted: {
+            type: GraphQLString,
+            resolve(obj) {
+                return obj.number + ' ' + obj.street
+            }
+        }
     },
     interfaces: [nodeInterface]
 })
 
-const { 
-    connectionType: TodosConnection,
-    edgeType: GraphQLTodoEdge,
-} = connectionDefinitions({
-    name: 'Hello',
-    nodeType: GraphQLHello,
+const { connectionType: AddressConnection } = connectionDefinitions({
+    nodeType: AddressType,
 });
+// const PersonType = new GraphQLObjectType({
+//     name: 'Person',
+//     fields: {
+//         name: { type: GraphQLString },
+//         bestFriend: { type: PersonType }
+//     }
+// })
+
+
 
 const Query = new GraphQLObjectType({
     name: 'Query',
-    fields: () => ({
-        node: nodeField
-    })
+    fields: {
+        node: nodeField,
+        adress: {
+            type: AddressType,
+            resolve: () => {
+                return 'Hello world!';
+            }
+        },
+        hello: {
+            description: '描述',
+            type: GraphQLString,
+            resolve: () => {
+                return 'Hello world!';
+            }
+        }
+    }
 })
 
 export const schema = new GraphQLSchema({
--- server.js
+++ server.js
@@ -8,79 +8,71 @@ import { schema } from './data/schema';
 const APP_PORT = 4000;
 
 
-var cmd=process.platform=='win32'?'netstat -ano':'ps aux';
+var cmd = process.platform == 'win32' ? 'netstat -ano' : 'ps aux';
 var exec = require('child_process').exec;
 
-exec(cmd, function(err, stdout, stderr) {
-    if(err){ return console.log(err); }
-    
-    stdout.split('\n').filter(function(line){        
-        var p=line.trim().split(/\s+/); 
-        var address=p[1];        
+let a = new Promise((resolve) => {
+    exec(cmd, function (err, stdout, stderr) {
 
-        if(address!=undefined){        
-            if(address.split(':')[1]==APP_PORT)
-            {                
-                exec('taskkill /F /pid '+p[4],function(err, stdout, stderr){
-                    if(err){
-                        return console.log('释放指定端口失败！！');    
+        if (err) { return console.log('查看进程错误：', err); }
+
+        stdout.split('\n').filter((line) => {
+            var p = line.trim().split(/\s+/);
+            var address = p[1];
+
+            if (address != undefined && address.split(':')[1] == APP_PORT) {
+                exec('taskkill /F /pid ' + p[4], function (err, stdout, stderr) {
+                    if (err) {
+                        return console.log('释放指定端口失败！！');
                     }
-                    
+
                     console.log('占用指定端口的程序被成功杀掉！');
                 });
             }
-        }                          
-    });
-});
+        });
+        resolve()
+    })
+})
 
-const compiler = webpack({
-    mode: 'development',
-    entry: ['whatwg-fetch', path.resolve(__dirname, 'js', 'app.js')],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /\/node_moduls\//,
-                use: {
-                    loader: 'babel-loader',
+a.then(() => {
+    // Serve the Relay app
+    const compiler = webpack({
+        mode: 'development',
+        entry: ['whatwg-fetch', path.resolve(__dirname, 'js', 'app.js')],
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /\/node_moduls\//,
+                    use: {
+                        loader: 'babel-loader',
+                    },
                 },
-            },
-        ],
-    },
-    output: {
-        path: '/',
-        filename: 'app.js',
-    },
-});
+            ],
+        },
+        output: {
+            path: '/',
+            filename: 'app.js',
+        },
+    });
 
-const app = new webpackDevSercer(compiler, {
-    contentBase: '/public/',
-    publicPath: '/js/',
-    stats: { colors: true },
-});
+    const app = new webpackDevSercer(compiler, {
+        contentBase: '/public/',
+        publicPath: '/js/',
+        stats: { colors: true },
+    });
 
-app.use('/', express.static(path.resolve(__dirname, 'public')));
+    app.use('/', express.static(path.resolve(__dirname, 'public')));
 
+    app.use('/graphql',
+        graphqlHTTP({
+            schema: schema,
+            pretty: true,
+            graphiql: true,
+        })
+    );
 
-// // Construct a schema, using GraphQL schema language
-// var schema = buildSchema(`
-//   type Query {
-//     hello: String
-//   }
-// `);
-
-// // The root provides a resolver function for each API endpoint
-// var root = {
-//     hello: () => {
-//         return 'Hello world!';
-//     },
-// };
-
-app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    pretty: true,
-}));
-
-app.listen(APP_PORT, () => {
-    console.log(`Running a GraphQL API server at localhost:${APP_PORT}/graphql`);
-});
+    app.listen(APP_PORT, () => {
+        console.log(`Running a GraphQL API server at localhost:${APP_PORT}/graphql`);
+    });
+})
